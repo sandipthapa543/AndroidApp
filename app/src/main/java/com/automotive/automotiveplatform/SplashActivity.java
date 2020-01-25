cd8a9d0aa7fec6cdae34912a322f497d950d8b12
Sandip Thapa <sandip.thapa543@gmail.com>
2020-01-25 23:49:09 +0545

    account setup

--- app/src/main/java/com/automotive/automotiveplatform/SplashActivity.java
+++ app/src/main/java/com/automotive/automotiveplatform/SplashActivity.java
@@ -4,15 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-<<<<<<< HEAD
-        setContentView(R.layout.activity_main);
-=======
-        setContentView(R.layout.activity_login);
->>>>>>> d3df10947f82fd2b5d0da75ee28d9cf257a89b48
+        setContentView(R.layout.activity_splash);
     }
 }
