180d003d6237756de715f80c8ce4f1bab8e78b02
Sandip Thapa <sandip.thapa543@gmail.com>
2020-01-26 19:19:28 +0545

    home page slider

--- app/src/main/java/com/automotive/automotiveplatform/ui/home/HomeFragment.java
+++ app/src/main/java/com/automotive/automotiveplatform/ui/home/HomeFragment.java
@@ -11,25 +11,21 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.automotive.automotiveplatform.R;
+import com.smarteist.autoimageslider.SliderView;
 
 public class HomeFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
+    SliderView sliderView;
+    RecyclerView recyclerView;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
+
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-       // final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-               // textView.setText(s);
-            }
-        });
-        return root;
+return root;
+
     }
 }
