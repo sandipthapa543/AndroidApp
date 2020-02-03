60691733454f058febdf45f433518c339b21cfea
Sandip Thapa <sandip.thapa543@gmail.com>
2020-02-04 00:57:32 +0545

    junit testing of user /me api and put api

--- app/src/test/java/com/automotive/automotiveplatform/AutomotiveUnitTest.java
+++ app/src/test/java/com/automotive/automotiveplatform/AutomotiveUnitTest.java
@@ -1,5 +1,7 @@
 package com.automotive.automotiveplatform;
 
+import com.google.android.gms.common.api.Api;
+
 import org.junit.Test;
 
 import api.ApiClass;
@@ -43,6 +45,36 @@ public class AutomotiveUnitTest {
                 e.printStackTrace();
             }
         }
+
+        // testing profile or /me api
+
+        @Test
+        public  void test_Profile(){
+            api =ApiClass.getInstance().create(UserApi.class);
+            Call<UserModel>userModelCall =api.getMe("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7InJvbGUiOiJjdXN0b21lciIsIl9pZCI6IjVlMzg2Njk4M2EwMDFiMmMzY2MxMzBhNSIsImZpcnN0X05hbWUiOiJhZG1pbiIsImxhc3RfTmFtZSI6ImFsbGwiLCJlbWFpbCI6ImFza0BnbWFpbC5jb20iLCJwYXNzd29yZCI6IiQyYSQxMCRpZmROSFpzY1Q2aWtpUHBSc1BrdC9lSVh2MGU2bnJpVUQwSXFwYUZYNFo3NXBRUjlnbXg5LiIsInBob25lIjoiOTAxNDEwMzY4OSIsImFkZHJlc3MiOiJwa3IiLCJfX3YiOjB9LCJpYXQiOjE1ODA3NTQ2MjcsImV4cCI6MTU4MDg0MTAyN30.wXMSdeuLjuyl97qFUAd3VY1ZXxsw6F5oUJbWpoQBIJY");
+        try{
+            Response<UserModel> userModelResponse =userModelCall.execute();
+            assertTrue(userModelResponse.isSuccessful());
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        }
+
+        // testing updateProfile or put api
+       @Test
+      public  void updateProfile(){
+            api = ApiClass.getInstance().create(UserApi.class);
+            Call<UserModel>userModelCall=api.updateuser("5e3866983a001b2c3cc130a5","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7InJvbGUiOiJjdXN0b21lciIsIl9pZCI6IjVlMzg2Njk4M2EwMDFiMmMzY2MxMzBhNSIsImZpcnN0X05hbWUiOiJhZG1pbiIsImxhc3RfTmFtZSI6ImFsbGwiLCJlbWFpbCI6ImFza0BnbWFpbC5jb20iLCJwYXNzd29yZCI6IiQyYSQxMCRpZmROSFpzY1Q2aWtpUHBSc1BrdC9lSVh2MGU2bnJpVUQwSXFwYUZYNFo3NXBRUjlnbXg5LiIsInBob25lIjoiOTAxNDEwMzY4OSIsImFkZHJlc3MiOiJwa3IiLCJfX3YiOjB9LCJpYXQiOjE1ODA3NTQ2MjcsImV4cCI6MTU4MDg0MTAyN30.wXMSdeuLjuyl97qFUAd3VY1ZXxsw6F5oUJbWpoQBIJY",
+                    "sandip","thapa","912103699","pokhara");
+            try{
+                Response<UserModel>userModelResponse=userModelCall.execute();
+                assertTrue(userModelResponse.isSuccessful());
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
+       }
 
     }
 
