410e542f750283536cd482927dab2342f7853116
Sandip Thapa <sandip.thapa543@gmail.com>
2020-02-11 01:02:52 +0545

    home adapter and product adapter created

--- app/src/main/java/model/UserModel.java
+++ app/src/main/java/model/UserModel.java
@@ -1,14 +1,17 @@
 package model;
 
 public class UserModel {
-    private String _id, first_Name,last_Name,email,password,phone,address;
+    private String _id, role, first_Name,last_Name,email,password,phone,address;
 
-    public String get_id() {
-        return _id;
-    }
-
-    public void set_id(String _id) {
+    public UserModel(String _id, String role, String first_Name, String last_Name, String email, String password, String phone, String address) {
         this._id = _id;
+        this.role = role;
+        this.first_Name = first_Name;
+        this.last_Name = last_Name;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+        this.address = address;
     }
 
     public UserModel(String first_Name, String last_Name, String email, String password, String phone, String address) {
@@ -20,51 +23,35 @@ public class UserModel {
         this.address = address;
     }
 
-    public String getFirst_Name() {
-        return first_Name;
+    public String get_id() {
+        return _id;
     }
 
-    public void setFirst_Name(String first_Name) {
-        this.first_Name = first_Name;
+    public String getRole() {
+        return role;
+    }
+
+    public String getFirst_Name() {
+        return first_Name;
     }
 
     public String getLast_Name() {
         return last_Name;
     }
 
-    public void setLast_Name(String last_Name) {
-        this.last_Name = last_Name;
-    }
-
     public String getEmail() {
         return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 
     public String getPassword() {
         return password;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
     public String getPhone() {
         return phone;
     }
 
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
     public String getAddress() {
         return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
     }
 }
