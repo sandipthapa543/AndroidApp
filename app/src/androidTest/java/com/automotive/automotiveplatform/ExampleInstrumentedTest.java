2f95d8532531d6a074a074975267db2126ab160a
Sandip Thapa <sandip.thapa543@gmail.com>
2020-02-12 01:00:48 +0545

    instrumental testing

--- app/src/androidTest/java/com/automotive/automotiveplatform/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/automotive/automotiveplatform/ExampleInstrumentedTest.java
@@ -2,10 +2,13 @@ package com.automotive.automotiveplatform;
 
 import android.content.Context;
 
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.test.filters.LargeTest;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Before;
 import org.junit.runner.RunWith;
 
 
@@ -13,8 +16,12 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.concurrent.RecursiveAction;
+
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.swipeLeft;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.*;
@@ -26,7 +33,9 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-
+    @Rule
+    public ActivityTestRule<DashboardActivity>
+            testRule = new ActivityTestRule<>(DashboardActivity.class);
 
     @Test
     public void useAppContext() {
@@ -37,23 +46,65 @@ public class ExampleInstrumentedTest {
     }
 
 
-        @Rule
-        public ActivityTestRule<LoginActivity>
-                testRule = new ActivityTestRule<>(LoginActivity.class);
 
-        @Test
-        public void checkNos()
-        {
-            onView(withId(R.id.login_emailid))
-                    .perform(typeText("ask@gmail.com"));
-            onView(withId(R.id.login_password))
-                    .perform(typeText("admin1234"));
-            onView(withId(R.id.btnLogin))
-                    .perform(click());
-
-            // This is another activity, no need to tell Espresso
+    @Before
+    public void fragment(){
+        testRule.getActivity().getSupportFragmentManager().beginTransaction();
 
 
-        }
+    }
+
+    @Test
+    public void home(){
+        onView(withId(R.id.navigation_home))
+                .perform(swipeLeft());
+        onView(withId(R.id.navigation_Acc))
+        .perform(click());
+    }
+
+    @Test
+    public void profile(){
+        onView(withId(R.id.navigation_dashboard))
+                .perform(click());
+    }
+
+    @Test
+    public void editProfile(){
+        onView(withId(R.id.navigation_Acc))
+                .perform(click());
+        onView(withId(R.id.btnEdit))
+                .perform(click());
+        onView((withId(R.id.firstName)))
+                .perform(typeText("sandip"));
+        onView(withId(R.id.lastName))
+                .perform(typeText("thapa"));
+        onView(withId(R.id.Phone))
+                .perform(typeText("9814103679"),closeSoftKeyboard());
+        onView(withId(R.id.Address))
+                .perform(typeText("pokhara"),closeSoftKeyboard());
+        onView(withId(R.id.btnUpdate))
+                .perform(click());
+    }
+
+//    @Test
+//    public void signUp(){
+//        onView(withId(R.id.firstName))
+//                .perform(click
+//    }
+
+//        @Test
+//        public void checkNos()
+//        {
+//            onView(withId(R.id.login_emailid))
+//                    .perform(typeText("ask@gmail.com"),closeSoftKeyboard());
+//            onView(withId(R.id.login_password))
+//                    .perform(typeText("admin1234"),closeSoftKeyboard());
+//            onView(withId(R.id.btnLogin))
+//                    .perform(click());
+//
+//            // This is another activity, no need to tell Espresso
+//
+//
+//        }
     }
 
